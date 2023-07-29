4dde354ef2ea48e6e4d2583dcbcad1aa6bab8b82
unknown <824351081@qq.com>
2023-07-29 21:21:08 +0800

    Docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'OpenLAMM', // Usually your GitHub org/user name.
   projectName: 'openlamm.github.io/', // Usually your repo name.
-  deploymentBranch: 'docusaurus',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
