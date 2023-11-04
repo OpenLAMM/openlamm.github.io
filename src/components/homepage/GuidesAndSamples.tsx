53adfe78fad82bd0a3cecea3412dd86ccc6c9a94
ZHELUN SHI <shizhelun20000820@126.com>
2023-11-04 18:49:07 +0800

    add arxiv link + readme

--- src/components/homepage/GuidesAndSamples.tsx
+++ src/components/homepage/GuidesAndSamples.tsx
@@ -12,6 +12,7 @@ import clsx from 'clsx';
 import { ChevronRight, GitHub } from 'react-feather';
 
 interface PAPER {
+  id: string;
   title: string;
   image: any;
   author: string;
@@ -21,6 +22,7 @@ interface PAPER {
 
 const publications: PAPER[] = [
   {
+    id:'LAMM',
     title: 'LAMM: Language-Assisted Multi-Modal Instruction-Tuning Dataset, Framework, and Benchmark',
     image: '/img/LAMM.png',
     author: 'Zhenfei Yin*, Jiong Wang*, JianJian Cao*, Zhelun Shi*,  Dingning Liu, Mukai Li, Lu Sheng, Xiaoshui Huang, Lei Bai†, Zhiyong Wang, Wanli Ouyang, Jing Shao†',
@@ -31,6 +33,7 @@ const publications: PAPER[] = [
 
 const preprint: PAPER[] = [
   {
+    id:'ChEF',
     title: 'ChEF: A Comprehensive Evaluation Framework for Standardized Assessment of Multimodal Large Language Models',
     image: '/img/ChEF.png',
     author: 'Zhelun Shi*, Zhipin Wang*, Hongxing Fan*, Zhenfei Yin, Lu Sheng†, Jing Shao, Yu Qiao',
@@ -38,6 +41,7 @@ const preprint: PAPER[] = [
     link: 'https://arxiv.org/abs/2306.06687',
   },
   {
+    id:'Octavius',
     title: 'Octavius: Mitigating Task Interference in MLLMs via MoE',
     image: '/img/LAMM.png',
     author: 'Zeren Chen*, Ziqin Wang*, Zhen Wang*, Huayang Liu, Zhenfei Yin, Si Liu, Lu Sheng†, Wanli Ouyang, Jing Shao',
@@ -47,10 +51,10 @@ const preprint: PAPER[] = [
 ];
 
 
-function Publish({ title, image,  author, pub,  link }: (typeof publications)[0]) {
+function Publish({id, title, image,  author, pub,  link }: (typeof publications)[0]) {
   return (
     <Link
-      to={link}
+      to={'/paper_list/'+id}
       className="group flex cursor-pointer items-start gap-2 rounded-lg border-2 border-transparent p-3 text-inherit transition-colors hover:border-primary hover:text-primary"
     >
       <img src={image} className="paper_image" />
@@ -59,6 +63,7 @@ function Publish({ title, image,  author, pub,  link }: (typeof publications)[0]
         <h4 className="mb-1 font-semibold">{title}</h4>
         <p className="mb-0 text-sm text-text-400">{author}</p>
         <i className="mb-0 text-sm text-text-400">{pub}</i>
+        <a className='mb-0 text-text-400' href={link}>Arxiv Link</a>
       </div>
 
       <ChevronRight className="ml-auto h-5 w-5 self-center opacity-0 transition-opacity group-hover:opacity-100" />
